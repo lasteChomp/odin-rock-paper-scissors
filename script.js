e503f205cbc6778dc524492e6d609ad2f326f16f
lasteChomp <163031341+lasteChomp@users.noreply.github.com>
2025-10-29 17:15:51 +0300

    feat(script.js): Add a playGame function to play the rounds more times than one
    - Created a for loop to play the rounds 5 times by calling the playRound function
    - After the for loop, log the related messages to the console by scores

--- script.js
+++ script.js
@@ -32,33 +32,57 @@ function getHumanChoice() {
 
 function playRound(humanChoice, computerChoice) {
   if (humanChoice === computerChoice) {
-    return `Draw! You both played ${humanChoice}. No points to anyone!`;
+    return `Draw! You both played ${humanChoice}. No points to anyone!\n\nYour score: ${humanScore}, Computer's score: ${computerScore}`;
   } else if (humanChoice === "rock") {
     if (computerChoice === "paper") {
       computerScore++;
-      return `You chose: ${humanChoice} and the computer chose: ${computerChoice}. You lose! Paper beats Rock. One point to the computer!`;
+      return `You chose: ${humanChoice} and the computer chose: ${computerChoice}. You lose! Paper beats Rock. One point to the computer!\n\nYour score: ${humanScore}, Computer's score: ${computerScore}`;
     } else {
       humanScore++;
-      return `You chose: ${humanChoice} and the computer chose: ${computerChoice}. You win! Rock beats Scissors. One point to you!`;
+      return `You chose: ${humanChoice} and the computer chose: ${computerChoice}. You win! Rock beats Scissors. One point to you!\n\nYour score: ${humanScore}, Computer's score: ${computerScore}`;
     }
   } else if (humanChoice === "paper") {
     if (computerChoice === "scissors") {
       computerScore++;
-      return `You chose: ${humanChoice} and the computer chose: ${computerChoice}. You lose! Scissors beats Paper. One point to the computer!`;
+      return `You chose: ${humanChoice} and the computer chose: ${computerChoice}. You lose! Scissors beats Paper. One point to the computer!\n\nYour score: ${humanScore}, Computer's score: ${computerScore}`;
     } else {
       humanScore++;
-      return `You chose: ${humanChoice} and the computer chose: ${computerChoice}. You win! Paper beats Rock. One point to you!`;
+      return `You chose: ${humanChoice} and the computer chose: ${computerChoice}. You win! Paper beats Rock. One point to you!\n\nYour score: ${humanScore}, Computer's score: ${computerScore}`;
     }
   } else if (humanChoice === "scissors") {
     if (computerChoice === "rock") {
       computerScore++;
-      return `You chose: ${humanChoice} and the computer chose: ${computerChoice}. You lose! Rock beats Scissors. One point to the computer!`;
+      return `You chose: ${humanChoice} and the computer chose: ${computerChoice}. You lose! Rock beats Scissors. One point to the computer!\n\nYour score: ${humanScore}, Computer's score: ${computerScore}`;
     } else {
       humanScore++;
-      return `You chose: ${humanChoice} and the computer chose: ${computerChoice}. You win! Scissors beats Paper. One point to you!`;
+      return `You chose: ${humanChoice} and the computer chose: ${computerChoice}. You win! Scissors beats Paper. One point to you!\n\nYour score: ${humanScore}, Computer's score: ${computerScore}`;
     }
+  }
+}
+
+function playGame() {
+  for (let i = 0; i < 5; i++) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    console.log(playRound(humanSelection, computerSelection));
+  }
+
+  if (humanScore === computerScore) {
+    console.log(
+      `Draw!\n\nYour final score: ${humanScore}, Computer's final score: ${computerScore}`
+    );
+  } else if (humanScore > computerScore) {
+    console.log(
+      `You won!\n\nYour final score: ${humanScore}, Computer's final score: ${computerScore}`
+    );
+  } else {
+    console.log(
+      `You lost!\n\nYour final score: ${humanScore}, Computer's final score: ${computerScore}`
+    );
   }
 }
 
 let humanScore = 0;
 let computerScore = 0;
+
+playGame();
